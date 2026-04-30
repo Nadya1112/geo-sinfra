37f7c4dfaf1c3ef30992c11a567741eeb19b2989
Nadya1112 <nadiabjm412@gmail.com>
2026-04-30 16:05:20 +0800

    fix(user): restore missing SoftDeletes and HasMany imports

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
