5164556a00b60be36ebb24ac0e3dfcfa177de180
Farhan Basahir <farhanbasahir@gmail.com>
2026-08-25 17:13:23 +0800

    feat: implement modular Livewire data tables and structural layout components (by Farhan)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -115,7 +115,7 @@
         @endif
     @endif
 
-    <main class="flex-1 overflow-y-auto custom-scrollbar text-left flex flex-col h-screen pb-24 md:pb-0 relative z-10">
+    <main class="flex-1 overflow-y-auto custom-scrollbar text-left flex flex-col h-screen pb-28 md:pb-0 relative z-10">
         @include('partials.header')
 
         <div class="p-4 md:p-8 flex-1">
